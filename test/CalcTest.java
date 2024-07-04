beacd1235ed693d5a37d7609d9e8b4b5f028f4f1
rocha30 <144186626+rocha30@users.noreply.github.com>
2024-07-04 11:43:28 -0600

    Clase CalcTest

--- test/CalcTest.java
+++ test/CalcTest.java
@@ -1,3 +1,31 @@
+package test;
+
+import org.junit.Test;
+import src.Calc;
+import static org.junit.Assert.assertEquals;
+
 public class CalcTest {
-    
+
+    @Test
+    public void testSuma() {
+        Calc calc = new Calc();
+        assertEquals(5, calc.suma(2, 3));
+        assertEquals(-1, calc.suma(2, -3));
+        assertEquals(0, calc.suma(0, 0));
+    }
+
+    @Test
+    public void testResta() {
+        Calc calc = new Calc();
+        assertEquals(1, calc.resta(3, 2));
+        assertEquals(5, calc.resta(2, -3));
+        assertEquals(0, calc.resta(0, 0));
+    }
+
+    @Test
+    public void testMultiplicacion() {
+        Calc calc = new Calc();
+        assertEquals(6, calc.multiplicacion(2, 3));
+        assertEquals(-6, calc.multiplicacion(2, -3));
+        assertEquals(0, calc.multiplicacion(0,3))}
 }
