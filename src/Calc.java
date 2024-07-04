0d2619370cbcac82beac376d93bfe335c3fc98b5
Ian Cumes <87866288+iancumes@users.noreply.github.com>
2024-07-04 11:33:37 -0600

    Calculadora

---
+++ src/Calc.java
@@ -0,0 +1,13 @@
+public class Calc{
+    public  int suma(int n1, int n2){
+        return n1 + n2;
+    }
+    
+    public  int resta(int n1, int n2){
+        return n1 - n2;
+    }
+    
+    public  int multiplicacion(int n1, int n2){
+        return n1 * n2;
+    }
+}
